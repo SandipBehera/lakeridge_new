2fd02a1bb85b7d86425a76653b91699bf6a2f14b
SandipBehera <sandip.sudip36@gmail.com>
2025-02-01 20:11:21 +0530

    cahnged number and price

--- src/app/price/price.component.ts
+++ src/app/price/price.component.ts
@@ -11,5 +11,11 @@ export class PriceComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  callEnquiry() {
+    window.location.href = 'tel:+919182786926'; // Replace with your phone number
+  }
 
+  chatOnWhatsApp(size: string) {
+    window.open(`https://api.whatsapp.com/send?phone=+919182786926&text=Hi!%20I'm%20I%20want%20to%20know%20more%20about%20${size}%20in%20Sattva%20Lakeridge.%20Please%20share%20details`, '_blank'); // Replace with your WhatsApp number
+  }
 }
